5a1806cefe42027d46de4565adb82ab8aca70437
yukinakai <10libra20.y@gmail.com>
2025-02-19 17:48:55 +0900

    chore: Jestの設定を更新し、TypeScriptのサポートを追加

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@ module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFiles: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|@react-native-community|@react-navigation)/)',
+  ],
+  testEnvironment: 'node',
 };
