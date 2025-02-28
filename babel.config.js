6dda0589aeb814623f3f4a9ffa60b050cfea2847
yukinakai <10libra20.y@gmail.com>
2025-02-28 15:17:16 +0900

    first commit

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  env: {
-    test: {
-      plugins: ['@babel/plugin-transform-modules-commonjs'],
-    },
-  },
-};
--- jest.setup.js
+++
@@ -1,5 +0,0 @@
-// Learn more: https://github.com/testing-library/jest-dom
-import '@jest/globals';
-import dotenv from 'dotenv';
-
-dotenv.config();
