8c95666a9b3a3cfb6412237b94d7b9859bebfd4c
yukinakai <10libra20.y@gmail.com>
2025-03-06 22:01:47 +0900

    @testing-library/react-nativeに移行ou

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,4 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
   ],
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
 }
