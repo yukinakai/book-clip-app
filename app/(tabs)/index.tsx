308e247553b15e72c88e420b9f2a1d7bc444e1e2
yukinakai <10libra20.y@gmail.com>
2025-03-06 18:45:33 +0900

    chore

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { MOCK_BOOKS } from '../../constants/MockData';
 import BookshelfView from '../../components/BookshelfView';
-import { useRouter } from 'expo-router';
+
+interface Book {
+  id: string;
+  title: string;
+}
 
 export default function HomeScreen() {
-  const router = useRouter();
-
-  const handleBookSelect = (book) => {
+  const handleBookSelect = (book: Book) => {
     // In the future, this would navigate to a book detail screen
     console.log('Selected book:', book.title);
     // Example: router.push(`/book-details/${book.id}`);
