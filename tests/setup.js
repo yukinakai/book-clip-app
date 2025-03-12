5855d36fb8ce73159430f6b71782cbb6159557a8
yukinakai <10libra20.y@gmail.com>
2025-03-12 19:47:36 +0900

    setup.jsを修正し、AsyncStorageのモックを直接定義

--- tests/setup.js
+++ tests/setup.js
@@ -1,6 +1,14 @@
 // テスト環境のセットアップ
-jest.mock("@react-native-async-storage/async-storage", () =>
-  require("./__mocks__/@react-native-async-storage/async-storage")
-);
+jest.mock("@react-native-async-storage/async-storage", () => ({
+  setItem: jest.fn(() => Promise.resolve()),
+  getItem: jest.fn(() => Promise.resolve(null)),
+  removeItem: jest.fn(() => Promise.resolve()),
+  clear: jest.fn(() => Promise.resolve()),
+  getAllKeys: jest.fn(() => Promise.resolve([])),
+  multiGet: jest.fn(() => Promise.resolve([])),
+  multiSet: jest.fn(() => Promise.resolve()),
+  multiRemove: jest.fn(() => Promise.resolve()),
+  mergeItem: jest.fn(() => Promise.resolve()),
+}));
 
 // 他のモックやグローバル設定をここに追加
