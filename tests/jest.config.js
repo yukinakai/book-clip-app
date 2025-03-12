ef63b5106edfa320e066ab96474ceddd59fbb91d
yukinakai <10libra20.y@gmail.com>
2025-03-12 19:45:03 +0900

    Jest設定ファイルを修正し、テスト実行パスを設定

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  preset: 'jest-expo',
+  preset: "jest-expo",
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
-}
+  testMatch: ["<rootDir>/**/*.test.tsx", "<rootDir>/**/*-test.tsx"],
+  rootDir: "..",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+};
