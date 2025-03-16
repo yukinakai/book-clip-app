ab009804a7ea482f2bb108a1cbd1b3cf27621fbc
yukinakai <10libra20.y@gmail.com>
2025-03-17 00:49:56 +0900

    refactor: @testing-library/react-hooksから@testing-library/reactへ移行してReact 18互換性を改善

--- tests/hooks/useBookScanner.test.tsx
+++ tests/hooks/useBookScanner.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react";
 import { Alert } from "react-native";
 import { useBookScanner } from "../../hooks/useBookScanner";
 
