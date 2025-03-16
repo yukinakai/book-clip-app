24188489a0bbd64d3ab4944a6cc6a4127fd44500
yukinakai <10libra20.y@gmail.com>
2025-03-16 11:14:12 +0900

    ESLintとunused-importsプラグインを追加し、設定を構成

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: "expo/native",
+  parser: "@typescript-eslint/parser",
+  plugins: ["unused-imports"],
+  rules: {
+    "no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
