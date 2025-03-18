1107ef00927431f533a38d01989ed4ca03df0dcd
yukinakai <10libra20.y@gmail.com>
2025-03-18 18:06:49 +0900

    fix: Imageコンポーネントのインポート漏れを修正

--- app/book/select.tsx
+++ app/book/select.tsx
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
   SafeAreaView,
   ActivityIndicator,
+  Image,
 } from "react-native";
 import { useRouter } from "expo-router";
 import { Book } from "../../constants/MockData";
